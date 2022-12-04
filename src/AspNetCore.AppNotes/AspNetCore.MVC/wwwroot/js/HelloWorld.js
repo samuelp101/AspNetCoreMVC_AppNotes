031ee81f5db971590bde5052db593f735a820673
samuelp101 <samuelphung@live.com>
2022-12-04 13:32:32 -0800

    TypeScript support added

---
+++ src/AspNetCore.AppNotes/AspNetCore.MVC/wwwroot/js/HelloWorld.js
@@ -0,0 +1,6 @@
+function HelloWorld() {
+    console.log("Hello World from TypeScript!");
+    document.getElementById("msg")
+        .innerHTML = "<h1>Hello World!</h1>";
+}
+//# sourceMappingURL=HelloWorld.js.map
---
+++ src/AspNetCore.AppNotes/AspNetCore.MVC/wwwroot/typescript/HelloWorld.ts
@@ -0,0 +1,5 @@
+function HelloWorld() {
+    console.log("Hello World from TypeScript!");
+    document.getElementById("msg")
+        .innerHTML = "<h1>Hello World!</h1>";
+}
